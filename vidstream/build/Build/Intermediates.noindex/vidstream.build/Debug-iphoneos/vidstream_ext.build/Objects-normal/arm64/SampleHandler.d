0b0b80fcd03cd746ff7abac2e4156ff9da7efbfb
rohithmone <monerohith@lambdatest.com>
2022-01-21 23:48:53 +0530

    Insatlled Setup

---
+++ vidstream/build/Build/Intermediates.noindex/vidstream.build/Debug-iphoneos/vidstream_ext.build/Objects-normal/arm64/SampleHandler.d
@@ -0,0 +1,25 @@
+dependencies: \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/SampleHandler.m \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/SampleHandler.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/kcdata.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/uuid.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/netinet6.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/device.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/mach_debug.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/mach_machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/mach.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/bank.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/darwin_posix_sys_types.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/netinet.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/net.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/darwin_cdefs.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/nng/nng.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/nng/protocol/pipeline0/push.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/nng/protocol/pipeline0/pull.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/nng/protocol/reqrep0/rep.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/nng/protocol/reqrep0/req.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/ujsonin/ujsonin.h \
+  /Users/monerohith/Documents/RemoteControl/ios_video_app/vidstream/vidstream_ext/ujsonin/string-tree.h
